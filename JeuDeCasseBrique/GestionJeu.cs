8fb50ecf61ff521a81617b6476517b73534f76f5
201830439 <201830439@cstfelicien.qc.ca>
2021-04-14 16:11:55 -0400

    Ajoutez des fichiers projet.

---
+++ JeuDeCasseBrique/GestionJeu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace JeuDeCasseBrique
+{
+    class GestionJeu
+    {
+        #region Attriuts
+        GameWindow window;
+        #endregion
+
+        #region ConstructeurInitialisation
+        public GestionJeu(GameWindow window)
+        {
+            this.window = window;
+        }
+        #endregion
+         private void start()
+        {
+            double nbrIPS = 60.0;
+            double dureeAffichageCHaqueImage = 1.0 / nbrIPS;
+
+            window.Run(dureeAffichageCHaqueImage);
+
+        }
+    }
+}
---
+++ JeuDeCasseBrique/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace JeuDeCasseBrique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Attributs
+            int largeurFenetre = 1080;
+            int hauteurFenetre = 720;
+            string titreFenetre = "Projet casse-brique";
+            #endregion //Attributs
+
+            #region code
+            GameWindow window = new GameWindow(largeurFenetre, hauteurFenetre, GraphicsMode.Default, titreFenetre);
+            GestionJeu fenetrePrincipale = new GestionJeu(window);
+            #endregion //code
+        }
+    }
+}
