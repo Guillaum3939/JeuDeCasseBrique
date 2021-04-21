45d57b64b8ffcb607a2d5dbd83114ca6a7d6a18a
lydiaDub52 <71521608+lydiaDub52@users.noreply.github.com>
2021-04-21 14:07:42 -0400

    base pour image

--- JeuDeCasseBrique/BasePourObjets.cs
+++ JeuDeCasseBrique/BasePourObjets.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows;
 using OpenTK.Graphics.OpenGL;
+using System.Drawing.Imaging;
 using System.Drawing;
 using OpenTK;
 
@@ -20,6 +21,7 @@ namespace JeuDeCasseBrique
         protected string nomTexture;
         #endregion //Attributs
 
+        #region ConstructeurInitialisation
         public BasePourObjets(string nomTexture, Vector2 a, Vector2 b, Vector2 c, Vector2 d)
         {
             this.listePoints = new Vector2[4];
@@ -31,6 +33,47 @@ namespace JeuDeCasseBrique
             init(nomTexture);
         }
 
+        private void init(string nomTexture)
+        {
+            this.nomTexture = nomTexture;
+            chargerTexture();
+        }
+
+
+        #endregion //Constructeur & Initialisation
+
+        #region Gestion Texture
+        private void chargerTexture()
+        {
+            GL.GenTextures(1, out textureID);
+            GL.BindTexture(TextureTarget.Texture2D, textureID);
+            BitmapData textureData = chargerImage();
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, textureData.Width, textureData.Height, 0,
+                                        OpenTK.Graphics.OpenGL.PixelFormat.Bgr, PixelType.UnsignedByte, textureData.Scan0);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
+
+        private BitmapData chargerImage()
+        {
+            Bitmap bmpImage = new Bitmap(nomTexture);
+            Rectangle rectangle = new Rectangle(0, 0, bmpImage.Width, bmpImage.Height);
+            BitmapData bmpData = bmpImage.LockBits(rectangle, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            bmpImage.UnlockBits(bmpData);
+            return bmpData;
+        }
+
+       
+        public void setCoordonneeTextureBrique()
+        {
+            coordonneesTextures = new Vector2[4];
+            coordonneesTextures[0] = new Vector2(0.0f, 1.0f);
+            coordonneesTextures[1] = new Vector2(1.0f, 1.0f);
+            coordonneesTextures[2] = new Vector2(1.0f, 0.0f);
+            coordonneesTextures[3] = new Vector2(0.0f, 0.0f);
+        }
+
+
+        #endregion //Gestion Texture
 
 
     }
