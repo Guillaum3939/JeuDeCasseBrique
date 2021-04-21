94e88783655fe264c239a404ef8219b364f6c03d
lydiaDub52 <71521608+lydiaDub52@users.noreply.github.com>
2021-04-21 14:01:13 -0400

    base pour objet

--- JeuDeCasseBrique/BasePourObjets.cs
+++ JeuDeCasseBrique/BasePourObjets.cs
@@ -5,12 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using OpenTK;
 
 namespace JeuDeCasseBrique
 {
     class BasePourObjets
     {
+        #region Attributs
+        protected Vector2[] listePoints;
+        protected Vector2[] coordonneesTextures;
+        protected int textureID;
+        protected string nomTexture;
+        #endregion //Attributs
 
-       
+        public BasePourObjets(string nomTexture, Vector2 a, Vector2 b, Vector2 c, Vector2 d)
+        {
+            this.listePoints = new Vector2[4];
+            this.listePoints[0] = a;
+            this.listePoints[1] = b;
+            this.listePoints[2] = c;
+            this.listePoints[3] = d;
+            setCoordonneeTextureBrique();
+            init(nomTexture);
+        }
+
+
+
     }
 }
--- JeuDeCasseBrique/Brique.cs
+++ JeuDeCasseBrique/Brique.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
 
 namespace JeuDeCasseBrique
 {
-    class Brique
+    class Brique : BasePourObjets
     {
+
+        #region Attributs
+        //Rien a mettre ici
+        public float incrementVertical = 1.5f;
+        public float deplacementVertical = 0.0f;
+        public float incrementHorizontale = 2.0f;
+        public float deplacementHorizontale = 0.0f;
+        int valDomage;
+        #endregion //Attributs
+
+        public Brique(string nomTexture, Vector2 a, Vector2 b, Vector2 c, Vector2 d) : base(nomTexture, a, b, c, d)
+        {
+            
+        }
+
+
     }
 }
